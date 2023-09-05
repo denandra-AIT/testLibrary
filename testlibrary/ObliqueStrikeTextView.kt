54d411b6379921abef9c6a90334fc3f4cac8e3ce
Denandra Prasetya <denandra1628@gmail.com>
2023-09-06 00:16:40 +0700

    first commit

---
+++ testlibrary/ObliqueStrikeTextView.kt
@@ -0,0 +1,44 @@
+package com.example.diagonalstriketrough
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.widget.TextView
+
+@SuppressLint("AppCompatCustomView")
+class ObliqueStrikeTextView : TextView {
+    private var dividerColor = 0
+    private var paint: Paint? = null
+
+    constructor(context: Context) : super(context) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init(context)
+    }
+
+    private fun init(context: Context) {
+        val resources = context.resources
+        //replace with your color
+        dividerColor = resources.getColor(R.color.black);
+        paint = Paint()
+        paint!!.color = dividerColor
+        paint!!.strokeWidth = 4f
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawLine(-30f, height.toFloat(), width.toFloat(), 10f, paint!!)
+    }
+}
---
+++ testlibrary/ToasterMessage.kt
@@ -0,0 +1,13 @@
+package com.example.diagonalstriketrough
+
+import android.content.Context
+import android.widget.Toast
+
+
+object ToasterMessage {
+
+    fun s(c: Context?, message: String?) {
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+    }
+
+}
